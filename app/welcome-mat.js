4b32e6cb475ef928c890e9f3152275f392e3af93
Mark Winterbottom <mark@londonappdeveloper.com>
2017-02-14 09:04:18 +0000

    Added scroll blocker. Added timer. Added skeleton functions for shouldShow and markShown.

--- app/welcome-mat.js
+++ app/welcome-mat.js
@@ -1,7 +1,44 @@
 var WelcomeMat = (function() {
     var pubs = {};
 
+    var delay = 2000;
+    var displayed = false;
 
+    /**
+     * Test if the user has already once closed or completed signup.
+    */
+    function shouldShow() {
+
+    }
+
+    /**
+     * Marks the mat as already displayed.
+    */
+    function markShown() {
+
+    }
+
+    /**
+     * Displays the welcome mat.
+    */
+    function showWelcomeMat() {
+        $('html, body').scrollTop(0);
+        $('div.lad-welcome-mat').show();
+        $('html').addClass('lad-welcome-mat-block-scroll');
+    }
+
+    /**
+     * Initialize the application.
+    */
+    pubs.init = function init() {
+        console.log('Initialize.');
+        setTimeout(function() {
+            console.log('BOOM!');
+            showWelcomeMat();
+        }, delay);
+    };
 
     return pubs;
 })();
+
+WelcomeMat.init();
