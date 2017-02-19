fe539f8a1c39c0833ac501e3951a2f9e5767168a
Mark Winterbottom <mark@londonappdeveloper.com>
2017-02-19 15:46:51 +0000

    Added logic to submit to MailChimp.

--- app/email-amass.js
+++ app/email-amass.js
@@ -28,6 +28,39 @@ var EmailAmass = (function() {
     }
 
     /**
+     * Checks if a form is valid or not.
+     * @function validateForm
+     * @param {Object} $form Form object to validate.
+    */
+    function validateForm($form) {
+        // TODO: Add email validation.
+        return true;
+    }
+
+    /**
+     * Submits a users email form to MailChimp.
+     * @function submitEmail
+     * @param {Object} $form Form object to validate.
+    */
+    function submitEmail($form) {
+        $.ajax({
+            type: $form.attr('method'),
+            url: $form.attr('action'),
+            data: $form.serialize(),
+            cache: false,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        })
+        .then(function(res) {
+            console.log('Registration successful.', res);
+        })
+        .catch(function(err) {
+            console.log('Registration failed.');
+            console.log(err);
+        });
+    }
+
+    /**
      * Initialize the application.
     */
     pubs.init = function init() {
@@ -36,6 +69,17 @@ var EmailAmass = (function() {
             console.log('BOOM!');
             showFullPageEmailForm();
         }, delay);
+
+        var $form = $('#email-amass-mailchimp-form');
+        if ($form.length > 0) {
+            $('#email-amass-mailchimp-form input[type="submit"]').bind('click', function registerClicked(event) {
+                if (event) event.preventDefault();
+                if (validateForm($form)) {
+                    submitEmail($form);
+                }
+            });
+        }
+
     };
 
     return pubs;
